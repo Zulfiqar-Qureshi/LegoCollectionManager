86c94b0913a2500da003ddacd0f7842ee7ac13ed
Zulfiqar Ali <40031007+Zulfiqar-Qureshi@users.noreply.github.com>
2021-03-02 18:54:30 +0500

    removed unused import from this file

--- backend/controllers/users/UserAuth.middleware.ts
+++ backend/controllers/users/UserAuth.middleware.ts
@@ -1,12 +1,12 @@
 import {Request, Response, NextFunction} from 'express';
 import jwt from 'jsonwebtoken';
-import connection from "../../database_connection";
+import {Token_encodeInterface} from "./token_encode.interface";
 
 export default (req: Request, res: Response, next: NextFunction)=>{
     const {token} = req.cookies;
     try{
         //@ts-ignore
-        jwt.verify(token, process.env.PRIVATE_KEY, (err, decoded) => {
+        jwt.verify(token, process.env.PRIVATE_KEY, (err, decoded: Token_encodeInterface) => {
             if(err){
                 res.json({
                     code: 500,
